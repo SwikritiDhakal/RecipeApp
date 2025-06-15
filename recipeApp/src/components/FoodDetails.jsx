ddb0d9f36d81230299b167c9c0886e6f225ddba1
Swikriti Dhakal <151549393+SwikritiDhakal@users.noreply.github.com>
2025-06-15 16:43:42 +0545

    styling

--- recipeApp/src/components/FoodDetails.jsx
+++ recipeApp/src/components/FoodDetails.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import styles from './fooddetails.module.css'
+
 
 export default function FoodDetails({ foodId }) {
   const API_KEY = "83d04b585c99420da81fdb931ba58623";
@@ -27,11 +29,11 @@ export default function FoodDetails({ foodId }) {
   }, [foodId]);
 
   return (
-    <div>
-      <div>
-         <h1>{food.title}</h1>
-         <img src={food.image} alt="" />
-      </div>
+    <div className={styles.mainContainer}>
+      <div className="foodTitle">
+         <h1 className={styles.foodName}>{food.title}</h1>
+         <img className={styles.recipeImage} src={food.image} alt="" />
+    <div className={styles.recipeDetails}>
       <span>
         <strong> Prep Time: {food.readyInMinutes} min 🕛</strong>
       </span>
@@ -39,23 +41,41 @@ export default function FoodDetails({ foodId }) {
         {food.vegetarian?"vegetarian 🫛🥕🍄‍🟫": "Non-vegetarian 🥩🍗"}
       </span>
       <span>
-         👩‍👧‍👦 serves {food.servings}
+         👩‍👧‍👦 serves :  {food.servings} people
       </span>
+      </div>
       <div>
       <span>
         💵💳 Price per serving: NRS. {food.pricePerServing} 
       </span>
 </div>
-
+<h2>REquired Ingredients 
+{food.extendedIngredients.map((item)=>
 <div>
-      <h2>  Instructions:</h2>
+
+<img src={`https://spoonacular.com/cdn/ingredients_100x100/`+item.image} alt="" />
+<h3>{item.name}</h3>
+  <h3>{item.amount}  {item.unit} </h3>
+
+</div>
+
+)}
+
+</h2>
+
+
+
+<h2>  Instructions:</h2>
+
+<div className={styles.recipeInstruction}>
+      <ol>
     {isLoading?( <p> Loading...</p> ): (food.analyzedInstructions[0].steps.map ((step)=>
  <li>{step.step} </li>
     ))
 
-   }
+   }'</ol>'
 </div>
-
+  </div>
 
     </div>
   );
